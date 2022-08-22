077103475dda745bc94073104888469895d57e28
Russell <russellm434@gmial.com>
2022-08-21 20:14:38 -0600

    finalización de tests

--- src/test/java/Fritri_Auto.java
+++ src/test/java/Fritri_Auto.java
@@ -51,7 +51,7 @@ public class Fritri_Auto {
         driver.findElementByXPath(" //android.widget.ScrollView[@content-desc=\"Block\"]/android.view.ViewGroup").click();
 
         //Create trip
-        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[4]/android.view.ViewGroup/android.view.View").click();
+        driver.findElementByXPath("(//android.widget.TextView[@content-desc=\"Text\"])[4]").click();
 
         driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[1]").click();
 
@@ -92,6 +92,103 @@ public class Fritri_Auto {
 
     }
 
+    @Test
+    public void createTrip() throws InterruptedException {
+    /*    //Click and pass Splash
+        driver.findElementByAccessibilityId("FRITRI-APP").click();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+*/
+        driver.findElementByXPath("(//android.widget.EditText[@content-desc=\"Input\"])[1]").click();
+        MobileElement mail = (MobileElement)  driver.findElementByXPath("(//android.widget.EditText[@content-desc=\"Input\"])[1]");
+        mail.sendKeys("antoniosmsn@hotmail.com");
+
+        driver.findElementByXPath(" //android.widget.ScrollView[@content-desc=\"Block\"]/android.view.ViewGroup").click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        MobileElement pass = (MobileElement)  driver.findElementByXPath("(//android.widget.EditText[@content-desc=\"Input\"])[2]");
+        pass.sendKeys("aaAA11");
+
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[3]/android.view.ViewGroup/android.view.View").click();
+        driver.findElementByXPath(" //android.widget.ScrollView[@content-desc=\"Block\"]/android.view.ViewGroup").click();
+
+        //Create trip
+        driver.findElementByXPath("(//android.widget.TextView[@content-desc=\"Text\"])[4]").click();
+
+        driver.findElementByXPath("(//android.widget.EditText[@content-desc=\"Input\"])[1]").click();
+        MobileElement name = (MobileElement)  driver.findElementByXPath("(//android.widget.EditText[@content-desc=\"Input\"])[1]");
+        name.sendKeys("TEST 4.5 QA");
+
+        driver.findElementByXPath("(//android.widget.EditText[@content-desc=\"Input\"])[2]").click();
+        MobileElement destino = (MobileElement)  driver.findElementByXPath("(//android.widget.EditText[@content-desc=\"Input\"])[2]");
+        destino.sendKeys("Limón Costa RIca");
+
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Block\"])[2]").click();
+
+
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[3]/android.view.ViewGroup/android.view.View").click();
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToBeginning(100000)"));
+
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Block\"])[13]/android.view.ViewGroup/android.view.ViewGroup").click();
+
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[3]/android.view.ViewGroup/android.view.View").click();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Block\"])[17]/android.view.ViewGroup/android.view.ViewGroup").click();
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[2]/android.view.ViewGroup/android.view.View").click();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Block\"])[17]/android.view.ViewGroup/android.view.ViewGroup").click();
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[2]/android.view.ViewGroup/android.view.View").click();
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[2]/android.view.ViewGroup/android.view.View").click();
+
+        driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button").click();
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+        driver.findElementByXPath("(//android.widget.ImageView[@content-desc=\"Image\"])[6]").click();
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[4]/android.view.ViewGroup/android.view.View").click();
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[5]/android.view.ViewGroup/android.view.View").click();
+        driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]").click();
+
+        //Scroll
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[3]/android.view.ViewGroup/android.view.View").click();
+        driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]").click();
+
+        driver.navigate().back();
+
+
+
+
+         /* driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[6]").click();
+        driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[1]").click();
+
+
+
+        //Scrolls
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToBeginning(100000)"));
+
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+
+        //LOgout
+     driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"Button\"])[6]").click();
+        driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[1]").click();
+*/
+    }
 
 
     @AfterMethod
